af123ddd20f86d2865d9e5d9a68e6812f9356189
ADMİN <ADMN@DESKTOP-BFTQN8H>
2022-08-10 00:23:48 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository eFCategoryRepository;
+        public CategoryManager()
+        {
+            eFCategoryRepository = new EFCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            eFCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            eFCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            eFCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return eFCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return eFCategoryRepository.GetListAll();
+        }
+    }
+}
